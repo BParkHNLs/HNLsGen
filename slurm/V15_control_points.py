60ab2af27da4a1c36133e4e7b95571065514cb0f
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2020-12-17 20:06:47 +0100

    correction

--- slurm/V15_control_points.py
+++ slurm/V15_control_points.py
@@ -4,7 +4,7 @@ from python.common  import Point,Config,getCtauEff
 #   - N.B.: mass must be a float
 
 p = Point(mass=999,ctau=999,vv=999)
-cfg = Config(nevtseff=300000,muoneff=2.0e-02,displeff=1.0,timeevt=100,timejob=35,contingency=3.)
+cfg = Config(nevtseff=300000,muoneff=2.0e-02,displeff=1.0,timeevt=100,timejob=35,contingency=6.)
 p.setConfig(cfg)
 
 points = []
