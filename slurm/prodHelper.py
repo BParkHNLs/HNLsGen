82f54af96b901831236977655f3d64fdfb03395a
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2021-06-14 11:16:41 +0200

    now can write also Bc fragments and updated B fragments

--- slurm/prodHelper.py
+++ slurm/prodHelper.py
@@ -492,7 +492,9 @@ process.BToNMuXFilter = cms.EDFilter("PythiaFilterMotherSister",
     ParticleID = cms.untracked.int32(13),
     MotherIDs = cms.untracked.vint32(521, 511, 531), 
     SisterID = cms.untracked.int32(9900015), 
-    MaxSisterDisplacement = cms.untracked.double(2900.), # max Lxyz displacement to generate in mm, -1 for no max
+    MaxSisterDisplacement = cms.untracked.double(1300.), # max Lxy displacement to generate in mm, -1 for no max
+    NephewIDs = cms.untracked.vint32(11,13,211), # ids of the nephews you want to check the pt of
+    MinNephewPts = cms.untracked.vdouble(0.4,0.4,0.5), 
 )
 
 process.generator = cms.EDFilter("Pythia8GeneratorFilter",
@@ -547,6 +549,89 @@ process.ProductionFilterSequence = cms.Sequence(process.generator+process.BFilte
     print('')
     print('===> Wrote the fragments \n')
 
+  def writeFragmentsBc(self):
+    for p in self.points:
+      fragname = 'BcToNMuX_NToEMuPi_SoftQCD_b_mN{:.1f}_ctau{:.1f}mm_TuneCP5_13TeV_pythia8-evtgen_cfi.py'.format(p.mass,p.ctau)
+      with open('{}/{}'.format(self.prodLabel,fragname), 'w') as f:
+        tobewritten = '''
+import FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.Eras import eras
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('Bc -> mu N X, with long-lived N, m={MASS:.1f}GeV, ctau={CTAU:.1f}mm'),
+    name = cms.untracked.string('Bc -> mu N X, with long-lived N, m={MASS:.1f}GeV, ctau={CTAU:.1f}mm'),
+    version = cms.untracked.string('$1.0$')
+)
+
+process.BFilter = cms.EDFilter("MCMultiParticleFilter",
+   NumRequired = cms.int32(1),
+   AcceptMore = cms.bool(True),
+   ParticleID = cms.vint32(541),
+   PtMin = cms.vdouble(0.),
+   EtaMax = cms.vdouble(10.),
+   Status = cms.vint32(0), 
+)
+
+process.BToNMuXFilter = cms.EDFilter("PythiaFilterMotherSister", 
+    MaxEta = cms.untracked.double(1.55),
+    MinEta = cms.untracked.double(-1.55),
+    MinPt = cms.untracked.double(6.8), 
+    ParticleID = cms.untracked.int32(13),
+    MotherIDs = cms.untracked.vint32(541), 
+    SisterID = cms.untracked.int32(9900015), 
+    MaxSisterDisplacement = cms.untracked.double(1300.), # max Lxy displacement to generate in mm, -1 for no max
+    NephewIDs = cms.untracked.vint32(11,13,211), # ids of the nephews you want to check the pt of
+    MinNephewPts = cms.untracked.vdouble(0.4,0.4,0.5), 
+)
+
+process.generator = cms.EDFilter("Pythia8HadronizerFilter",
+    ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            convertPythiaCodes = cms.untracked.bool(False),
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
+            
+            list_forced_decays = cms.vstring(       
+                'myBc+', 
+                'myBc-',
+            ),
+            
+            operates_on_particles = cms.vint32(541, -541), 
+            particle_property_file = cms.FileInPath('HNLsGen/evtGenData/evt_2014_mass{MASS:.1f}_ctau{CTAU:.1f}_maj.pdl'),
+            user_decay_file = cms.vstring('HNLsGen/evtGenData/HNLdecay_mass{MASS:.1f}_maj_emu_Bc.DEC'),
+        ),
+        parameterSets = cms.vstring('EvtGen130')
+    ),
+
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring('541:m0 = 6.275',
+                                        '541:tau0 = 0.153',
+                                        ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+    ) 
+
+    comEnergy = cms.double(13000.0),
+    filterEfficiency = cms.untracked.double(-1),      # this will not be used by Pythia, only saved in GenInfo 
+    crossSection = cms.double(1.0)
+    maxEventsToPrint = cms.untracked.int32(0),        
+    pythiaHepMCVerbosity = cms.untracked.bool(False), 
+    pythiaPylistVerbosity = cms.untracked.int32(0)    
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator+process.BFilter+process.BToNMuXFilter)
+
+'''.format(MASS=p.mass,CTAU=p.ctau)
+        f.write(tobewritten)
+    print('')
+    print('===> Wrote the fragments \n')
 
 
 
@@ -607,7 +692,11 @@ if __name__ == "__main__":
   job.writeCfg()   
 
   if opt.dofragments:
-    job.writeFragments()
+    if opt.dobc:
+      job.writeFragmentsBc()
+    else:
+      job.writeFragments()
+   
 
   if opt.dosubmit:
     job.submit()
